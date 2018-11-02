86c97ab375c24fffb60a0d30ab1512a159df9bf8
Matthew J. Morrison <matt@ascentus.com>
2018-11-02 01:11:58 -0400

    WIP

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-# import epd7in5
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+# main
 
+import epd7in5
 import datetime
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+EPD_WIDTH = 640
+EPD_HEIGHT = 384
 
 def main():
     fetchEvents()
@@ -52,8 +55,26 @@ def fetchEvents():
     #     print(start, event['summary'])
 
 def drawEvents():
-    for event in events:
-        print(event['summary'])
+    epd = epd7in5.EPD()
+    epd.init()
+
+    # For simplicity, the arguments are explicit numerical coordinates
+    image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)    # 1: clear the frame
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 24)
+    draw.rectangle((0, 6, 640, 30), fill = 0)
+    draw.text((200, 10), 'e-Paper demo', font = font, fill = 255)
+    draw.rectangle((200, 80, 600, 280), fill = 0)
+    draw.arc((240, 120, 580, 220), 0, 360, fill = 255)
+    draw.rectangle((0, 80, 160, 280), fill = 255)
+    draw.arc((40, 80, 180, 220), 0, 360, fill = 0)
+    epd.display_frame(epd.get_frame_buffer(image))
+
+    image = Image.open('monocolor.bmp')
+    epd.display_frame(epd.get_frame_buffer(image))
+
+    # You can get frame buffer from an image or import the buffer directly:
+    #epd.display_frame(imagedata.MONOCOLOR_BITMAP)
 
 if __name__ == '__main__':
     main()
